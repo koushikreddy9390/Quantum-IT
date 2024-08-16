6e9f7a0827cc01a79af6dfeda885d70769421f72
koushikreddy9390 <reddykoushik1642@gmail.com>
2024-08-16 11:30:50 +0530

    "updated"

--- src/Homepage.js
+++ src/Homepage.js
@@ -90,25 +90,28 @@ export default function Homepage() {
           </nav>
         </div>
       </div>
+    
       <div className="container">
-  <div className="card">
-    <div className="card-body">
-      <h5 className="card-title">WE MAKE UR IDEA'S</h5>
-      <h5 className="card-subtitle mb-2 text-body-secondary">INTO REALITY</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the
-        bulk of the card's content.
-      </p>
-      <button className="card-link">Get started</button>
-      <button className="card-link">Another link</button>
-    </div>
-    <img
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr-xocv-pROiMF7AL89ogcqX7HEmmQ9n1alg&s"
-      alt="pic1"
-      className="image"
-    />
-  </div>
-</div>
+        <div className="card" style={{ border: "none" }}>
+          <div className="card-body">
+            <h5 className="card-title">WE MAKE UR IDEA'S</h5>
+            <h5 className="card-subtitle mb-2 text-body-secondary">
+              INTO REALITY
+            </h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <button className="card-link">Get started</button>
+            <button className="card-link">Another link</button>
+          </div>
+        </div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr-xocv-pROiMF7AL89ogcqX7HEmmQ9n1alg&s"
+          alt="pic1"
+          className="image"
+        />
+      </div>
 
 
       <div>
